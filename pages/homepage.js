c6e69635a1b28612fb549d93b5ba78d8f88d0606
michaelbrinkworth <55236695+michaelbrinkworth@users.noreply.github.com>
2023-05-03 14:26:05 +1000

    trial

--- pages/homepage.js
+++ pages/homepage.js
@@ -3,7 +3,8 @@ import ContentfulImage from '../components/contentful-image'
 
 function HomePage({ content }) {
     const heroImage = content[0].fields.heroImage;
-  
+    
+  console.log(content)
     return (
       <div>
         <div style={{ height: '600', width: '100%' }}>
@@ -11,11 +12,15 @@ function HomePage({ content }) {
             src={heroImage.fields.file.url}
             layout='fill'
             objectFit='contain'
+            alt='hero'
           />
         </div>
         {content.map(entry => (
           <div key={entry.sys.id}>
             <p>{entry.fields.MissionStatement}</p>
+            <p>{entry.fields.block1body}</p>
+            <p>{entry.fields.block2body}</p>
+            <p>{entry.fields.block1Body}</p>
           </div>
         ))}
       </div>
